6b99281373bbc1653726022a8892a727d359973a
Urbinam7 <L24580324@reynosa.tecnm.mx>
2025-05-12 23:22:50 -0500

    Add files via upload

---
+++ Control de un Brazo Robótico 3D con Interfaz Gráfica en Visual C/2/Form1.Designer.cs
@@ -0,0 +1,252 @@
+﻿namespace _2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            trackBarArt1 = new TrackBar();
+            trackBarArt2 = new TrackBar();
+            trackBarArt3 = new TrackBar();
+            trackBarBase = new TrackBar();
+            trackBargripper = new TrackBar();
+            label1 = new Label();
+            label2 = new Label();
+            button1 = new Button();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            Buttonguardavalores = new Button();
+            buttonreprovalores = new Button();
+            button2 = new Button();
+            ((System.ComponentModel.ISupportInitialize)trackBarArt1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)trackBarArt2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)trackBarArt3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)trackBarBase).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)trackBargripper).BeginInit();
+            SuspendLayout();
+            // 
+            // trackBarArt1
+            // 
+            trackBarArt1.BackColor = Color.White;
+            trackBarArt1.Location = new Point(149, 21);
+            trackBarArt1.Maximum = 180;
+            trackBarArt1.Name = "trackBarArt1";
+            trackBarArt1.Size = new Size(350, 69);
+            trackBarArt1.TabIndex = 0;
+            trackBarArt1.TickFrequency = 10;
+            trackBarArt1.Value = 90;
+            trackBarArt1.Scroll += trackBarArt1_Scroll;
+            // 
+            // trackBarArt2
+            // 
+            trackBarArt2.BackColor = Color.White;
+            trackBarArt2.Location = new Point(149, 119);
+            trackBarArt2.Maximum = 180;
+            trackBarArt2.Name = "trackBarArt2";
+            trackBarArt2.Size = new Size(350, 69);
+            trackBarArt2.TabIndex = 1;
+            trackBarArt2.TickFrequency = 10;
+            trackBarArt2.Value = 90;
+            trackBarArt2.Scroll += trackBarArt2_Scroll;
+            // 
+            // trackBarArt3
+            // 
+            trackBarArt3.BackColor = Color.White;
+            trackBarArt3.Location = new Point(149, 215);
+            trackBarArt3.Maximum = 180;
+            trackBarArt3.Name = "trackBarArt3";
+            trackBarArt3.Size = new Size(350, 69);
+            trackBarArt3.TabIndex = 2;
+            trackBarArt3.TickFrequency = 10;
+            trackBarArt3.Value = 90;
+            trackBarArt3.Scroll += trackBarArt3_Scroll;
+            // 
+            // trackBarBase
+            // 
+            trackBarBase.BackColor = Color.White;
+            trackBarBase.Location = new Point(149, 313);
+            trackBarBase.Maximum = 180;
+            trackBarBase.Name = "trackBarBase";
+            trackBarBase.Size = new Size(350, 69);
+            trackBarBase.TabIndex = 3;
+            trackBarBase.TickFrequency = 10;
+            trackBarBase.Value = 90;
+            trackBarBase.Scroll += trackBarBase_Scroll;
+            // 
+            // trackBargripper
+            // 
+            trackBargripper.BackColor = Color.White;
+            trackBargripper.Location = new Point(149, 410);
+            trackBargripper.Maximum = 90;
+            trackBargripper.Name = "trackBargripper";
+            trackBargripper.Size = new Size(350, 69);
+            trackBargripper.TabIndex = 4;
+            trackBargripper.TickFrequency = 10;
+            trackBargripper.Value = 45;
+            trackBargripper.Scroll += trackBargripper_Scroll;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(76, 329);
+            label1.Name = "label1";
+            label1.Size = new Size(48, 25);
+            label1.TabIndex = 5;
+            label1.Text = "Base";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(69, 421);
+            label2.Name = "label2";
+            label2.Size = new Size(71, 25);
+            label2.TabIndex = 6;
+            label2.Text = "Gripper";
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Lime;
+            button1.Location = new Point(62, 519);
+            button1.Name = "button1";
+            button1.Size = new Size(113, 62);
+            button1.TabIndex = 7;
+            button1.Text = "Origen";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(86, 38);
+            label3.Name = "label3";
+            label3.Size = new Size(38, 25);
+            label3.TabIndex = 8;
+            label3.Text = "M1";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(86, 137);
+            label4.Name = "label4";
+            label4.Size = new Size(38, 25);
+            label4.TabIndex = 9;
+            label4.Text = "M2";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(86, 236);
+            label5.Name = "label5";
+            label5.Size = new Size(38, 25);
+            label5.TabIndex = 10;
+            label5.Text = "M3";
+            // 
+            // Buttonguardavalores
+            // 
+            Buttonguardavalores.BackColor = Color.FromArgb(128, 255, 128);
+            Buttonguardavalores.Location = new Point(182, 519);
+            Buttonguardavalores.Name = "Buttonguardavalores";
+            Buttonguardavalores.Size = new Size(112, 62);
+            Buttonguardavalores.TabIndex = 12;
+            Buttonguardavalores.Text = "Guardar Posicion";
+            Buttonguardavalores.UseVisualStyleBackColor = false;
+            Buttonguardavalores.Click += button2_Click;
+            // 
+            // buttonreprovalores
+            // 
+            buttonreprovalores.BackColor = Color.FromArgb(128, 255, 128);
+            buttonreprovalores.Location = new Point(300, 519);
+            buttonreprovalores.Name = "buttonreprovalores";
+            buttonreprovalores.Size = new Size(123, 62);
+            buttonreprovalores.TabIndex = 13;
+            buttonreprovalores.Text = "Reproducir Posicion";
+            buttonreprovalores.UseVisualStyleBackColor = false;
+            buttonreprovalores.Click += buttonreprovalores_Click;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.Red;
+            button2.Location = new Point(529, 519);
+            button2.Name = "button2";
+            button2.Size = new Size(107, 62);
+            button2.TabIndex = 14;
+            button2.Text = "  Salir";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click_1;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ActiveBorder;
+            ClientSize = new Size(711, 639);
+            Controls.Add(button2);
+            Controls.Add(buttonreprovalores);
+            Controls.Add(Buttonguardavalores);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(button1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(trackBargripper);
+            Controls.Add(trackBarBase);
+            Controls.Add(trackBarArt3);
+            Controls.Add(trackBarArt2);
+            Controls.Add(trackBarArt1);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "Form1";
+            Text = "HMI BRAZO 2.0";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)trackBarArt1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)trackBarArt2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)trackBarArt3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)trackBarBase).EndInit();
+            ((System.ComponentModel.ISupportInitialize)trackBargripper).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TrackBar trackBarArt1;
+        private TrackBar trackBarArt2;
+        private TrackBar trackBarArt3;
+        private TrackBar trackBarBase;
+        private TrackBar trackBargripper;
+        private Label label1;
+        private Label label2;
+        private Button button1;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Button Buttonguardavalores;
+        private Button buttonreprovalores;
+        private Button button2;
+    }
+}
---
+++ Control de un Brazo Robótico 3D con Interfaz Gráfica en Visual C/2/Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.IO.Ports;
+using System.Windows.Forms;
+
+namespace _2
+{
+    public partial class Form1 : Form
+    {
+        // ✅ Declaración global de arduino
+        SerialPort arduino;
+
+        List<byte[]> posicionesGuardadas = new List<byte[]>();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Inicializa el puerto serial
+            arduino = new SerialPort("COM8", 115200);
+            try
+            {
+                arduino.Open();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al abrir el puerto COM8: " + ex.Message);
+            }
+        }
+
+        private void trackBarBase_Scroll(object sender, EventArgs e)
+        {
+            SendServoCommand(1, (byte)trackBarBase.Value);
+        }
+
+        private void trackBarArt1_Scroll(object sender, EventArgs e)
+        {
+            SendServoCommand(2, (byte)trackBarArt1.Value);
+        }
+
+        private void trackBarArt2_Scroll(object sender, EventArgs e)
+        {
+            SendServoCommand(3, (byte)trackBarArt2.Value);
+        }
+
+        private void trackBarArt3_Scroll(object sender, EventArgs e)
+        {
+            SendServoCommand(4, (byte)trackBarArt3.Value);
+        }
+
+        private void trackBargripper_Scroll(object sender, EventArgs e)
+        {
+            SendServoCommand(5, (byte)trackBargripper.Value);
+        }
+
+        // Método que envía comando al Arduino
+        private void SendServoCommand(byte id, byte angle)
+        {
+            if (arduino != null && arduino.IsOpen)
+            {
+                try
+                {
+                    byte[] data = new byte[] { id, angle };
+                    arduino.Write(data, 0, data.Length);
+                }
+                catch (Exception ex)
+                {
+
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Posiciones de origen sugeridas:
+            byte baseStop = 90;       // Detener servo continuo
+            byte artCenter = 90;      // Centro articulaciones
+            byte gripperStart = 45;   // Semicerrado
+
+            // Mueve los sliders visualmente
+            trackBarBase.Value = baseStop;
+            trackBarArt1.Value = artCenter;
+            trackBarArt2.Value = artCenter;
+            trackBarArt3.Value = artCenter;
+            trackBargripper.Value = gripperStart;
+
+            // Enviar comandos al Arduino
+            SendServoCommand(1, baseStop);
+            SendServoCommand(2, artCenter);
+            SendServoCommand(3, artCenter);
+            SendServoCommand(4, artCenter);
+            SendServoCommand(5, gripperStart);
+        }
+        // BOTÓN: GUARDAR POSICIÓN ACTUAL
+        private void button2_Click(object sender, EventArgs e)
+        {
+            byte[] nuevaPosicion = new byte[]
+    {
+        (byte)trackBarBase.Value,
+        (byte)trackBarArt1.Value,
+        (byte)trackBarArt2.Value,
+        (byte)trackBarArt3.Value,
+        (byte)trackBargripper.Value
+    };
+
+            posicionesGuardadas.Add(nuevaPosicion);
+            MessageBox.Show("¡Posición guardada!");
+        }
+        // BOTÓN: REP POSICIÓN ACTUAL
+        private void buttonreprovalores_Click(object sender, EventArgs e)
+        {
+
+            if (posicionesGuardadas.Count == 0)
+            {
+                MessageBox.Show("No hay posiciones guardadas.");
+                return;
+            }
+
+            // Ejemplo: reproducir la PRIMERA posición guardada
+            byte[] posicion = posicionesGuardadas[0];
+
+            trackBarBase.Value = posicion[0];
+            trackBarArt1.Value = posicion[1];
+            trackBarArt2.Value = posicion[2];
+            trackBarArt3.Value = posicion[3];
+            trackBargripper.Value = posicion[4];
+
+            SendServoCommand(1, posicion[0]);
+            SendServoCommand(2, posicion[1]);
+            SendServoCommand(3, posicion[2]);
+            SendServoCommand(4, posicion[3]);
+            SendServoCommand(5, posicion[4]);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+        
+        }
+    }
+}
